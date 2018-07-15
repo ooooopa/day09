469746d5b8c4747301edc4aad6b64f4fc827128f
Mayumi Hirano <opa0713@gmail.com>
2018-07-15 03:01:37 +0000

    init

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  resources :blogs
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
